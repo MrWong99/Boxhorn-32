cfede490c1cf4c85bf7d51f73fb4bc2184ce8ff0
Lukas Schmidt <Lukas.Schmidt@kisters.de>
2016-01-04 15:33:46 +0100

    Added Autofocus and Camera Light

---
+++ Boxhorn32/Assets/TollScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class TollScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        VuforiaBehaviour.Instance.RegisterVuforiaStartedCallback(abc);
+        VuforiaBehaviour.Instance.RegisterOnPauseCallback(xyz);
+	}
+	
+	private void abc()
+    {
+        CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        CameraDevice.Instance.SetFlashTorchMode(true);
+    }
+
+    private void xyz(bool paused)
+    {
+        if (!paused)
+        {
+            CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+            CameraDevice.Instance.SetFlashTorchMode(true);
+        }
+    }
+}
