32e5c68957f2589361aa10faeb765c432acbb83c
Lukas Schmidt <Lukas.Schmidt@kisters.de>
2016-01-05 10:46:45 +0100

    Minor fix for Focus and light script in ARCamera

---
+++ Boxhorn32/Assets/UserScripts/FocusAndLight.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class FocusAndLight : MonoBehaviour
+{
+
+    public bool useTorch;
+
+    public CameraDevice.FocusMode focusMode;
+
+    // Use this for initialization
+    void Start()
+    {
+        VuforiaBehaviour.Instance.RegisterVuforiaStartedCallback(abc);
+        VuforiaBehaviour.Instance.RegisterOnPauseCallback(xyz);
+    }
+
+    private void abc()
+    {
+        CameraDevice.Instance.SetFocusMode(focusMode);
+        CameraDevice.Instance.SetFlashTorchMode(useTorch);
+    }
+
+    private void xyz(bool paused)
+    {
+        if (!paused)
+        {
+            CameraDevice.Instance.SetFocusMode(focusMode);
+            CameraDevice.Instance.SetFlashTorchMode(useTorch);
+        }
+    }
+}
