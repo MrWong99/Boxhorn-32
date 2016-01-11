00fced4cd3f4e41bb7b5e5cafad9608418f07497
Lukas Schmidt <Lukas.Schmidt@kisters.de>
2016-01-11 15:58:48 +0100

    Added 3D modells and some scripts

---
+++ Boxhorn32/Assets/UserScripts/VideoPlaybackTrackableEventHandler.cs
@@ -0,0 +1,94 @@
+﻿/*==============================================================================
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Qualcomm Connected Experiences, Inc.
+==============================================================================*/
+
+using UnityEngine;
+
+namespace Vuforia
+{
+    /// <summary>
+    /// A custom handler that implements the ITrackableEventHandler interface.
+    /// </summary>
+    public class VideoPlaybackTrackableEventHandler : MonoBehaviour,
+                                                ITrackableEventHandler
+    {
+        #region PRIVATE_MEMBER_VARIABLES
+
+        private TrackableBehaviour mTrackableBehaviour;
+
+        #endregion // PRIVATE_MEMBER_VARIABLES
+
+        #region PUBLIC_MEMBER_VARIABLES
+
+        public string videoURL;
+
+        public Color bgColor;
+
+        public FullScreenMovieControlMode controlMode;
+
+        public FullScreenMovieScalingMode scalingMode;
+
+        #endregion PUBLIC_MEMBER_VARIABLES // PUBLIC_MEMBER_VARIABLES
+
+        #region UNTIY_MONOBEHAVIOUR_METHODS
+
+        void Start()
+        {
+            mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+            if (mTrackableBehaviour)
+            {
+                mTrackableBehaviour.RegisterTrackableEventHandler(this);
+            }
+        }
+
+        #endregion // UNTIY_MONOBEHAVIOUR_METHODS
+
+
+
+        #region PUBLIC_METHODS
+
+        /// <summary>
+        /// Implementation of the ITrackableEventHandler function called when the
+        /// tracking state changes.
+        /// </summary>
+        public void OnTrackableStateChanged(
+                                        TrackableBehaviour.Status previousStatus,
+                                        TrackableBehaviour.Status newStatus)
+        {
+            if (newStatus == TrackableBehaviour.Status.DETECTED ||
+                newStatus == TrackableBehaviour.Status.TRACKED ||
+                newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+            {
+                OnTrackingFound();
+            }
+            else
+            {
+                OnTrackingLost();
+            }
+        }
+
+        #endregion // PUBLIC_METHODS
+
+
+
+        #region PRIVATE_METHODS
+
+
+        private void OnTrackingFound()
+        {
+            Handheld.PlayFullScreenMovie(videoURL, bgColor, controlMode, scalingMode);
+
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+        }
+
+
+        private void OnTrackingLost()
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+        }
+
+        #endregion // PRIVATE_METHODS
+    }
+}
