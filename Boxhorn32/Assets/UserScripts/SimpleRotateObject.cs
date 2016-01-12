90cee7b787d12a792e3a6d883c66701b7263f9c7
Lukas Schmidt <Lukas.Schmidt@kisters.de>
2016-01-12 11:04:52 +0100

    Added rotation for objects

---
+++ Boxhorn32/Assets/UserScripts/SimpleRotateObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleRotateObject : MonoBehaviour {
+
+    // The speed at which the object should rotate
+    public float speed;
+
+    // The axis around where the object should be rotated
+    public Vector3 rotationAxis;
+
+    private Vector3 pivot;
+
+    void Start ()
+    {
+        pivot = transform.parent.position;
+    }
+    
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(pivot, rotationAxis, Time.deltaTime * speed);
+    }
+}
