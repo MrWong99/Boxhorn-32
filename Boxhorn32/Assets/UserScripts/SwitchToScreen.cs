31146a554a89d3c505422184257ae8a78fa8c8dc
Lukas Schmidt <Lukas.Schmidt@kisters.de>
2016-01-14 15:23:05 +0100

    Added script to be able to switch to another screen

---
+++ Boxhorn32/Assets/UserScripts/SwitchToScreen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchToScreen : MonoBehaviour
+{
+
+    public float SwitchAfterSeconds;
+
+    public GameObject ScreenToActivate;
+
+    void Start()
+    {
+        StartCoroutine(Routine());
+    }
+
+    private IEnumerator Routine()
+    {
+        yield return new WaitForSeconds(SwitchAfterSeconds);
+        gameObject.SetActive(false);
+        ScreenToActivate.SetActive(true);
+    }
+}
