dacb937a6af1872ed44cf09838ec09c07405ef2c
Lukas Schmidt <Lukas.Schmidt@kisters.de>
2016-01-15 14:20:40 +0100

    Added new rocket and some video's

--- Boxhorn32/Assets/UserScripts/RocketStartObject.cs
+++ Boxhorn32/Assets/UserScripts/RocketStartObject.cs
@@ -1,9 +1,11 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
-public class _RocketStart : MonoBehaviour
+public class RocketStartObject : MonoBehaviour
 {
-    public Rigidbody turbine;
+    public float FlightTime;
+    public Rigidbody Turbine;
+    public TextMesh CountdownText;
     Vector3 start;
     private float liftoff;
     private bool resetInitiated = false;
@@ -13,6 +15,7 @@ public class _RocketStart : MonoBehaviour
     {
         start = transform.position;
         liftoff = Time.time + 4.0f; // 4 Sekunden, initaler Timer
+        StartCoroutine(countdown(Time.time, liftoff));
     }
 
     // Update is called once per frame
@@ -21,7 +24,7 @@ public class _RocketStart : MonoBehaviour
         if (Time.time > liftoff)  //LIFTOFF
         {
             Debug.Log("LIFTOFF");
-            turbine.AddForce(Vector3.up * Time.deltaTime * 100000, ForceMode.Acceleration);
+            Turbine.AddForce(Vector3.up * Time.deltaTime * 100000, ForceMode.Acceleration);
             if (!resetInitiated)
             {
                 StartCoroutine(resetRocket()); // STOPS ROCKET X SECONDS AFTER LIFTOFF
@@ -30,13 +33,23 @@ public class _RocketStart : MonoBehaviour
         }
     }
 
+    IEnumerator countdown(float start, float end)
+    {
+        while (end > start)
+        {
+            CountdownText.text = "" + (int)(end - start);
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
     IEnumerator resetRocket()
     {
-        yield return new WaitForSeconds(2);
-        turbine.velocity = Vector3.zero;
+        yield return new WaitForSeconds(FlightTime);
+        Turbine.velocity = Vector3.zero;
         Debug.Log("RESET");
         liftoff = Time.time + 10.0f;	// 10 Sekunden zwischen weiteren Starts
         transform.position = start;
         resetInitiated = false;
+        StartCoroutine(countdown(Time.time, liftoff));
     }
 }
